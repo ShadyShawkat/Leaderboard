2441275f6014afc19e299e4200e3c3662165455d
ShadyShawkat <shadyshawkat91@gmail.com>
2021-12-15 21:53:52 +0200

    Implement user interactivity

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './style.css';
 import APIhelper from './APIHelper';
 
+const refreshBtn = document.getElementById('refresh-btn');
+const form = document.querySelector('form');
+const scoresList = document.querySelector('.scores__table ul');
+
+APIhelper.createGame();
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  APIhelper.postScore(form.name.value, form.score.value);
+});
+
+refreshBtn.addEventListener('click', async () => {
+  const data = await APIhelper.fetchScores();
+  if(data.result.length > 0) {
+    scoresList.classList.remove('d-none');
+  }
+  scoresList.innerHTML = data.result.map((obj) => {
+    return `<li>${obj.user}: ${obj.score}</li>`
+  }).join('');
+});
