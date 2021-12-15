19a9b45cdf357669376f1fd389a5e1f32dd62873
ShadyShawkat <shadyshawkat91@gmail.com>
2021-12-15 21:43:37 +0200

    Add helper class

---
+++ src/APIHelper.js
@@ -0,0 +1,46 @@
+let gameId = "";
+const apiURL = "https://us-central1-js-capstone-backend.cloudfunctions.net/api";
+
+const createGame = () => {
+  fetch(
+    `${apiURL}/games/`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        name: "My very cool new game",
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      [, , , gameId] = data.result.split(" ");
+    }); 
+  }
+
+const postScore = (name, score) => {
+  fetch(
+    `${apiURL}/games/${gameId}/scores`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        user: name,
+        score: score,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  )
+    .then((response) => response.json())
+}
+
+const fetchScores = async () => {
+  return await fetch(`${apiURL}/games/${gameId}/scores`).then((response) =>
+    response.json()
+  );
+};
+
+export default { fetchScores, createGame, postScore };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './style.css';
+import APIhelper from './APIHelper';
+
